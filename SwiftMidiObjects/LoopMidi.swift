004baa241b8077fd634f092885d834c1160973ed
Jim Ketcham <jsketcham@earthlink.net>
2025-07-24 12:04:50 -0700

    Added mic pre 3,4 to MIDI drop down

--- SwiftMidiObjects/LoopMidi.swift
+++ SwiftMidiObjects/LoopMidi.swift
@@ -16,6 +16,10 @@ class LoopMidi: NSObject {
     var swiftMidi2 : SwiftMidi?
     var swiftMidi3 : SwiftMidi?
     var swiftMidi4 : SwiftMidi?
+    var swiftMidi5 : SwiftMidi?
+    var swiftMidi6 : SwiftMidi?
+    var swiftMidi7 : SwiftMidi?
+    var swiftMidi8 : SwiftMidi?
 
     
     override init() {
@@ -25,6 +29,13 @@ class LoopMidi: NSObject {
         swiftMidi2 = SwiftMidi(self, "Mic Preamp 1 Return", .IN_AND_OUT); swiftMidi2?.loopInToOut = true
         swiftMidi3 = SwiftMidi(self, "Mic Preamp 2 Send", .IN_AND_OUT); swiftMidi3?.loopInToOut = true
         swiftMidi4 = SwiftMidi(self, "Mic Preamp 2 Return", .IN_AND_OUT); swiftMidi4?.loopInToOut = true
+        
+        // 07/24/25
+        swiftMidi5 = SwiftMidi(self, "Mic Preamp 3 Send", .IN_AND_OUT); swiftMidi5?.loopInToOut = true
+        swiftMidi6 = SwiftMidi(self, "Mic Preamp 3 Return", .IN_AND_OUT); swiftMidi6?.loopInToOut = true
+        swiftMidi7 = SwiftMidi(self, "Mic Preamp 4 Send", .IN_AND_OUT); swiftMidi7?.loopInToOut = true
+        swiftMidi8 = SwiftMidi(self, "Mic Preamp 4 Return", .IN_AND_OUT); swiftMidi8?.loopInToOut = true
+
 
     }
 
