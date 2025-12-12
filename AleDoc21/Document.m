710ad45f5c4fce467d861627f4534e2b06ec20f4
Jim Ketcham <jsketcham@earthlink.net>
2025-12-12 13:33:32 -0800

    1) backed up on the take/track clearing

--- AleDoc21/Document.m
+++ AleDoc21/Document.m
@@ -1919,12 +1919,12 @@ int m_retCode = NSModalResponseCancel;//NSCancelButton;  // initialize to someth
     // clear takes and tracks for _tableContents, will get filled in if there is a log entry
     
     // 12/12/25 leave items not in the log, alone
-//    for(NSMutableDictionary *dict in _tableContents){
-//        
-//        [dict setObject:@"0" forKey:@"Take"]; //NSLog(@"KEY  9");
-//        [dict setObject:@"1" forKey:@"Track"];
-//        
-//    }
+    for(NSMutableDictionary *dict in _tableContents){
+        
+        [dict setObject:@"0" forKey:@"Take"]; //NSLog(@"KEY  9");
+        [dict setObject:@"1" forKey:@"Track"];
+        
+    }
     
     if(![mgr fileExistsAtPath:pathToLog isDirectory:&isDir]){
         
